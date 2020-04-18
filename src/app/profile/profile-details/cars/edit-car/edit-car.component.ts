96b579754840d74072d8a5d0707a86c78140c2a2
Ivan Nomokonov <nomokonov.vana@gmail.com>
2020-04-18 17:25:52 +0300

    страница редактирования автомобиля

--- src/app/profile/profile-details/cars/edit-car/edit-car.component.ts
+++ src/app/profile/profile-details/cars/edit-car/edit-car.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bk-edit-car',
   templateUrl: './edit-car.component.html',
-  styleUrls: ['./edit-car.component.scss']
+  styleUrls: ['./edit-car.component.scss'],
 })
 export class EditCarComponent implements OnInit {
+  car = {
+    img: '../../../assets/cars/logan_new.jpeg',
+    name: 'Renault Logan II MT',
+    year: 2017,
+    description:
+      'Аренда Рено Логан. - демократичный и надёжный седан, с большим багажником и энергоёмкой подвеской. Оптимальное решение, если вы хотите получить хороший уровень комфорта по приемлемой цене.',
+    price: 1805,
+    engineType: 'Бензиновый, 16',
+    enginePower: 82,
+    speed: 172,
+    time: 11.9,
+    volumPerHundred: 7.2,
+    kpp: 'Механическая',
+    driveUnit: 'Передний',
+    place: 5,
+    backVolume: 510,
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
