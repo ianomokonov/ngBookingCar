f9d5f6308b2a27bae2b9abb5193df6bd0f50de02
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-05-02 21:02:27 +0300

    правки

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -207,44 +207,44 @@ import { FeedbackComponent } from './feedback/feedback.component';
               path: 'search',
               component: SearchComponent,
               data: {
-                title: 'Cheap Rent a car Crete / Car Hire in Crete with CAR4CRETE / Heraklion / Rethymnon / Chania ',
+                title: 'Rent a Car Crete 2022 / Hire a Car Crete 2022 / Car4Crete.com',
                 description:
-                  'Rent a car in Crete /Full insurance against all risks 100% no excess/ Free delivery at airport Heraklion and oll ower Crete',
+                  'Rent a car in Crete with Car4Crete. Hire a new car at Heraklion airport - no waiting in the line. Rent a vehicle in Heraklion port or hotel in Crete with best price. Family company, we are always in touch!',
                 keywords:
-                  'rent a car crete, rent a car heraklion, rent a car Rethymnon, rent a car Chania, crete car hire, rent a car heraklion crete, rent a car heraklion airport crete, rent a car heraklion port, rent a car heraklion greece, car rental in Crete Heraklion Airport ',
+                  'rent a car heraklion, car rental heraklion airport, car hire heraklion airport',
               },
             },
             {
               path: 'catalog',
               component: CatalogComponent,
               data: {
-                title: 'Car Hire in Heraklion airport Crete / Сhoose your car model with CAR4CRETE',
+                title: 'Car models for rent in Crete 2022/ Find the Best Price / Car4Crete.com',
                 description:
-                  'Crete: Rent cars of different brands, horsepower and configuration to adapt to your needs whether you are travelling as a couple, family or a group of friends',
+                  'The best car rental company in Crete will provide you with brand new Volkswagen, Fiat, Suzuki and Hyundai cars. Automatic, manual, hybrid and electric cars for rent in Crete. R ent a car Heraklion with Best Rates.',
                 keywords:
-                  'Hire car heraklion airport, Hire cars crete - heraklion airport, Best car hire heraklion airport, Cheap car hire crete heraklion airport, Hire a car in Crete, Car hire in crete greece, Car hire in crete hersonissos, Car hire in crete airport',
+                  'best car hire crete, rent a car crete prices, rent a car heraklion price, rent a car heraklion airport price',
               },
             },
             {
               path: 'contacts',
               component: ContactsComponent,
               data: {
-                title: 'Car rental company in Crete - CAR4CRETE',
+                title: 'Contact Car4Crete for rent a car Crete 2022 / Car4Crete.com',
                 description:
-                  'Booking a car in Crete with CAR4CRETE is very easy. Write online chat, call on telephone or Whats up, mail us. Rent a car Heraklion airport Crete, rent a car Heraklion port Crete. We are in touch 24/7.+30 69 4936 7278 info@carcrete24.com ',
+                  'Car Hire with Car4Crete is easy. Call us the car to delivered to the airport of Heraklion, Port or any Hotel of Crete ',
                 keywords:
-                  'Rent a car heraklion crete, rent a car heraklion airport crete, rent a car heraklion port, rent a car heraklion greece, car rental in Crete Heraklion Airport / rent a car deals with CAR4CRETE',
+                  'car hire heraklion airport, car rental crete heraklion airport, rent a car crete heraklion airport, car hire Heraklion, car rental amoudara crete, rent a car amoudara crete',
               },
             },
             {
               path: 'policy',
               component: PolicyComponent,
               data: {
-                title: 'Car rental company in Crete - CAR4CRETE',
+                title: 'Hire Car in Crete 2022 / Insurance policy / Car4Crete.com',
                 description:
-                  'Booking a car in Crete with CAR4CRETE is very easy. Write online chat, call on telephone or Whats up, mail us. Rent a car Heraklion airport Crete, rent a car Heraklion port Crete. We are in touch 24/7.+30 69 4936 7278 info@carcrete24.com ',
+                  'Conditions for car hire Crete Heraklion Rethimno Hersonissos. Premium car rental insurance for car hire Heraklion airport in Crete including 24 hour roadside assistance. Car rental company Car4Crete is an honest and reliable partner!',
                 keywords:
-                  'Rent a car heraklion crete, rent a car heraklion airport crete, rent a car heraklion port, rent a car heraklion greece, car rental in Crete Heraklion Airport / rent a car deals with CAR4CRETE',
+                  'car hire crete heraklion, car rental heraklion port, crete car hire heraklion airport',
               },
             },
             {
@@ -295,44 +295,44 @@ import { FeedbackComponent } from './feedback/feedback.component';
               path: 'search',
               component: SearchComponent,
               data: {
-                title: 'Cheap Rent a car Crete / Car Hire in Crete with CAR4CRETE / Heraklion / Rethymnon / Chania ',
+                title: 'Mietwagen Kreta 2022 / Autovermietung Kreta / Car4Crete.com',
                 description:
-                  'Rent a car in Crete /Full insurance against all risks 100% no excess/ Free delivery at airport Heraklion and oll ower Crete',
+                  'Die besten Mietwagenangebote auf Kreta. Vermietung am Flughafen Heraklion. Schnelle Autovermietung in Kreta Hotels. Familienunternehmen. Wir sind immer in Kontakt mit Ihnen!',
                 keywords:
-                  'rent a car crete, rent a car heraklion, rent a car Rethymnon, rent a car Chania, crete car hire, rent a car heraklion crete, rent a car heraklion airport crete, rent a car heraklion port, rent a car heraklion greece, car rental in Crete Heraklion Airport ',
+                  'auto mieten kreta, kreta mietwagen, mietwagen kreta heraklion flughafen, kreta auto mieten, mietwagen kreta heraklion, mietwagen kreta hotelzustellung',
               },
             },
             {
               path: 'catalog',
               component: CatalogComponent,
               data: {
-                title: 'Car Hire in Heraklion airport Crete / Сhoose your car model with CAR4CRETE',
+                title: 'Automodelle zu vermieten auf Kreta 2022 /Autovermietung in Heraklion, Rethymnon, Chania / Car4Crete.com',
                 description:
-                  'Crete: Rent cars of different brands, horsepower and configuration to adapt to your needs whether you are travelling as a couple, family or a group of friends',
+                  'Neue Autovermietung auf Kreta - Audi, VW, Fiat, Suzuki. Automatik, Schalt, Hybrid und Elektroautos zur Miete auf Kreta. Reservierung eines bestimmten Modells ohne Vorauszahlung. Preisgarantie ohne Zwischenhändler. Autovermietung in Heraklion, Heraklion Flughafen, Rethymnon, Hersonissos – einfach im Jahr 2022.',
                 keywords:
-                  'Hire car heraklion airport, Hire cars crete - heraklion airport, Best car hire heraklion airport, Cheap car hire crete heraklion airport, Hire a car in Crete, Car hire in crete greece, Car hire in crete hersonissos, Car hire in crete airport',
+                  'heraklion mietwagen flughafen, mietauto kreta heraklion flughafen, auto mieten in kreta, autovermietung flughafen heraklion,',
               },
             },
             {
               path: 'contacts',
               component: ContactsComponent,
               data: {
-                title: 'Car rental company in Crete - CAR4CRETE',
+                title: 'Kontaktieren Sie uns, um ein Auto auf Kreta zu mieten / Car4Crete.com',
                 description:
-                  'Booking a car in Crete with CAR4CRETE is very easy. Write online chat, call on telephone or Whats up, mail us. Rent a car Heraklion airport Crete, rent a car Heraklion port Crete. We are in touch 24/7.+30 69 4936 7278 info@carcrete24.com ',
+                  'Mietwagen Kreta 2022 - kontaktieren Sie uns direkt ohne Zeitverlust. Sie können ein Auto auf Kreta mieten, indem Sie anrufen, an den Messenger schreiben oder auf der Website buchen.',
                 keywords:
-                  'Rent a car heraklion crete, rent a car heraklion airport crete, rent a car heraklion port, rent a car heraklion greece, car rental in Crete Heraklion Airport / rent a car deals with CAR4CRETE',
+                  'billiger mietwagen kreta, auto mieten kreta flughafen, heraklion auto mieten, autovermietung kreta heraklion, mietwagen kreta hotelzustellung',
               },
             },
             {
               path: 'policy',
               component: PolicyComponent,
               data: {
-                title: 'Car rental company in Crete - CAR4CRETE',
+                title: 'Mietbedingung zum Auto auf Kreta mieten 2022 / Car4Crete.com',
                 description:
-                  'Booking a car in Crete with CAR4CRETE is very easy. Write online chat, call on telephone or Whats up, mail us. Rent a car Heraklion airport Crete, rent a car Heraklion port Crete. We are in touch 24/7.+30 69 4936 7278 info@carcrete24.com ',
+                  'Premium-Mietwagenversicherung auf Kreta – Sie entspannen sich, wir kümmern uns um Sie!',
                 keywords:
-                  'Rent a car heraklion crete, rent a car heraklion airport crete, rent a car heraklion port, rent a car heraklion greece, car rental in Crete Heraklion Airport / rent a car deals with CAR4CRETE',
+                  'auto auf kreta mieten, kreta autovermietung',
               },
             },
             {
@@ -348,9 +348,9 @@ import { FeedbackComponent } from './feedback/feedback.component';
               path: 'feedback',
               component: FeedbackComponent,
               data: {
-                title: 'What to see in Crete',
-                description: 'Holiday in Crete with car',
-                keywords: 'Crete car rental, Crete car hire',
+                title: 'Autovermietung auf Kreta - Mietstationen /  Car4Crete.com',
+                description: 'Sie können überall auf Kreta ein Auto mieten - direkt am Flughafen Heraklion oder zu Hotels mit Lieferung in Heraklion, Rethymno, Hersonissos, Malia, Anissaras, Bali. Wenn Sie auf Kreta ein Auto mieten, können Sie die Insel besser kennenlernen!',
+                keywords: 'autovermietung rethymno, autovermietung chersonissos, mietwagen kreta rethymno, autovermietung stalis kreta, autovermietung malia kreta, mietwagen agios nikolaos, mietwagen agia pelagia',
               },
             },
             { path: 'details/:id', component: DetailsComponent },
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bk-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  public today = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
